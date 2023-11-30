ffbf84e8b77bb8c967fada918d46a082d603ed6d
Pablo Pinto <pablohernanpinto@gmail.com>
2023-11-30 04:44:13 -0300

    ultimo avance pequeños cambios y en agregar institucion

--- src/app/components/modals/add-institucion/add-institucion.component.ts
+++ src/app/components/modals/add-institucion/add-institucion.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AddComComponent } from '../add-com/add-com.component';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, NgForm, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ModificarConvenioComponent } from '../modificar-convenio/modificar-convenio.component';
 
@@ -12,30 +12,46 @@ import { ModificarConvenioComponent } from '../modificar-convenio/modificar-conv
 })
 export class AddInstitucionComponent {
 
-  constructor(public dialogRef: MatDialogRef<AddComComponent>,private http: HttpClient) { }
+  constructor(public dialogRef: MatDialogRef<AddComComponent>,private http: HttpClient,private formBuilder: FormBuilder) { }
 
-  formulario = {
+
+
+  formulario = this.formBuilder.group({
     nombre_inst: '',
     pais: '',
     alcance: '',
     tipo_institucion: '',
-  };
+  })
+
+  inputValue: string = '';
+
+
+
+  Ver() {
+    console.log('Valor ingresado:', );
+    // Aquí puedes realizar la lógica que desees con el valor ingresado
+  }
 
   closeDialog() {
     this.dialogRef.close('');
     window.location.reload();
 
   }
+  enviarFormulario(){
+    console.log('Formulario enviado:', this.formulario);
+  }
 
-  addConvenio(formContact: NgForm) {
-    if (this.formulario.pais !== 'Chile'){
-      this.formulario.alcance = 'Internacional'
+
+  addConvenio() {
+    console.log(this.formulario.value)
+     if (this.formulario.value.pais !== 'Chile'){
+      this.formulario.value.alcance = 'Internacional'
     }
     else{
-      this.formulario.alcance = 'Nacional'
+      this.formulario.value.alcance = 'Nacional'
     }
-    if (formContact.valid) {
-      this.http.post('http://localhost:3000/api/instituciones', this.formulario).subscribe(
+    if (this.formulario.valid) {
+      this.http.post('http://localhost:3000/api/instituciones', this.formulario.value).subscribe(
           (data) => {
             alert('SE A INGRESADO INSTITUCION');
           //  window.location.reload();
@@ -52,7 +68,7 @@ export class AddInstitucionComponent {
         );
     } else {
       alert('INGRESO NO VÁLIDO');
-    }
+    } 
   }
 }
 
